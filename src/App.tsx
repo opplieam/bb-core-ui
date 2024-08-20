1a4c29c362f232c19f9eb9e4eeb2e2904c7d5004
Opp Lieam <runicer156@gmail.com>
2024-08-20 23:33:26 +0700

    feat(login): add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorNotFound from "./components/error/ErrorNotFound.tsx";
+import Login from "./components/login/Login.tsx";
 
 const router = createBrowserRouter([
   {
     path: "login",
-    element: <h1>Login</h1>
+    element: <Login />
   },
   {
     path: "*",
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,23 @@
+import { Container, Group, Paper, Text } from "@mantine/core";
+import { GoogleButton } from "./GoogleButton.tsx";
+
+function Login() {
+  return (
+    <>
+      <Container my={40} size={420}>
+        <Paper withBorder shadow="md" p={30} mt={30} radius={"md"}>
+          <Text size="lg" fw={400}>
+            Welcome to BuyBetter, Please login with
+          </Text>
+          <Group mb="md" mt="md">
+            <a href="/">
+              <GoogleButton radius="xl">Google</GoogleButton>
+            </a>
+          </Group>
+        </Paper>
+      </Container>
+    </>
+  );
+}
+
+export default Login;
